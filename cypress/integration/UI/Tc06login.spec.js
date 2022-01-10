60d6331f558a0ffebe10b7c289488887b234f897
Mahesh3232 <mahesh.aher1999@gmail.com>
2022-01-10 16:08:29 +0530

    Second Commit

---
+++ cypress/integration/UI/Tc06login.spec.js
@@ -0,0 +1,19 @@
+describe('Verify the login functionality',()=>{
+    it('Verify the login functionality with valid cardentials',()=>{
+        cy.visit('http://testphp.vulnweb.com/login.php')
+        cy.get('input[name="uname"]').type('test')
+        cy.get('input[name="pass"]').type('test')
+        cy.get('input[value="login"]').click()
+        cy.get('h2[id="pageName"]').should('be.visible')
+        cy.title().should('eq','user info')
+    })
+
+    it('Verify the login functionality with valid cardentials',()=>{
+        cy.visit('http://testphp.vulnweb.com/login.php')
+        cy.get('input[name="uname"]').type('test')
+        cy.get('input[name="pass"]').type('test123')
+        cy.get('input[value="login"]').click()
+        cy.get('img[alt="Acunetix website security"]').should('be.visible')
+        cy.title().should('eq','login page')
+    })
+})
