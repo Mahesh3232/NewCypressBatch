b60647a9a9369bd467b880ca3d6c5734d86e60a5
Mahesh3232 <mahesh.aher1999@gmail.com>
2022-01-11 14:17:30 +0530

    Third commit

---
+++ cypress/integration/UI/Tc08login.spec.js
@@ -0,0 +1,31 @@
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+        // returning false here prevents Cypress from
+        // failing the test
+        return false
+        })
+describe('verify the login functionality',()=>{
+    it('verify the login functionality with valid cardentials',()=>{
+        cy.visit('https://www.bewakoof.com/login')
+        cy.get('button[id="web_email_login"]').click()
+        cy.wait(300)
+        cy.get('input[id="email_input"]').type('mahesh.aher1999@gmail.com')
+        cy.get('input[id="mob_password"]').type('Sonu@3232')
+        cy.get('button[class="loginSubmit false"]').click()
+        cy.get('div[class="col-xs-2 mainHeaderCols bewakoofLogoDiv"]').should('be.visible')
+        cy.title('eq','Online Shopping for Men, Women Clothing & Accessories at Bewakoof')
+
+    })
+
+    it('verify the login functionality with valid cardentials',()=>{
+        cy.visit('https://www.bewakoof.com/login')
+        cy.get('button[id="web_email_login"]').click()
+        cy.wait(300)
+        cy.get('input[id="email_input"]').type('mahesh.aher1999@gmail.com')
+        cy.get('input[id="mob_password"]').type('Sonu@323232')
+        cy.get('button[class="loginSubmit false"]').click()
+        cy.get('span[id="mob_toast_error"]').should('be.visible')
+        cy.title('eq','Online Shopping for Men, Women Clothing & Accessories at Bewakoof')
+
+    })
+})
