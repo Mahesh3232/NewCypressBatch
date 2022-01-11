cb672a00a388ab74986032d5ce53263f7a88b5b6
Mahesh3232 <mahesh.aher1999@gmail.com>
2022-01-11 16:08:21 +0530

    Foutth commit

--- cypress/integration/UI/Tc09login.spec.js
+++ cypress/integration/UI/Tc09login.spec.js
@@ -1,22 +1,22 @@
-describe('verify the login functionality',()=>{
+describe('verify the login functionality', () => {
 
-    it('verify the login functionality with valid cardentials',()=>{
+    it('verify the login functionality with valid cardentials', () => {
         cy.visit('https://livesql.oracle.com/apex/f?p=590:1000')
         cy.get('a[class="t-Button t-Button--icon t-Button--header t-Button--navBar"]').last().click()
         cy.get('input[id="sso_username"]').type('mahesh.aher1999@gmail.com')
         cy.get('input[id="ssopassword"]').type('Sonu@3232')
         cy.get('input[id="signin_button"]').click()
         cy.get('a[class="t-Header-logo-link"]').should('be.visible')
-        cy.title().should('eq','Oracle Live SQL')
+        cy.title().should('eq', 'Oracle Live SQL')
     })
 
-    it('verify the login functionality with invalid cardentials',()=>{
+    it('verify the login functionality with invalid cardentials', () => {
         cy.visit('https://livesql.oracle.com/apex/f?p=590:1000')
         cy.get('a[class="t-Button t-Button--icon t-Button--header t-Button--navBar"]').last().click()
         cy.get('input[id="sso_username"]').type('mahesh.aher1999@gmail.com')
         cy.get('input[id="ssopassword"]').type('Sonu@323232')
         cy.get('input[id="signin_button"]').click()
         cy.get('div[class="cb41error"]').last().should('be.visible')
-        cy.title().should('eq',' Oracle Login - Single Sign On')
+        cy.title().should('eq', ' Oracle Login - Single Sign On')
     })
 })
---
+++ cypress/integration/UI/Tc10login.spec.js
@@ -0,0 +1,21 @@
+
+
+describe('Verify the login functionality',()=>{
+    it('Verify login functionality with valid cardentials',()=>{
+        cy.visit('https://auth.testproject.io/auth/realms/TP/protocol/openid-connect/auth?client_id=tp.app&redirect_uri=https%3A%2F%2Fapp.testproject.io%2Fcallback.html&response_type=id_token%20token&scope=openid%20profile&state=28efaf1fda084e12bf48cf4114c7225d&nonce=1bba86461c914c48b391f14ce8bbf6c9')
+        cy.get('input[id="username"]').type('mahesh.aher1999@gmail.com')
+        cy.get('input[id="password"]').type('Sonu@3232')
+        cy.get('input[id="tp-sign-in"]').click()
+        cy.wait(20000)
+        cy.get('svg[class="logo"]').should('be.visible')
+    })
+
+    it('Verify login functionality with invalid cardentials',()=>{
+        cy.visit('https://auth.testproject.io/auth/realms/TP/protocol/openid-connect/auth?client_id=tp.app&redirect_uri=https%3A%2F%2Fapp.testproject.io%2Fcallback.html&response_type=id_token%20token&scope=openid%20profile&state=28efaf1fda084e12bf48cf4114c7225d&nonce=1bba86461c914c48b391f14ce8bbf6c9')
+        cy.get('input[id="username"]').type('mahesh.aher1999@gmail.com')
+        cy.get('input[id="password"]').type('Sonu@323243')
+        cy.get('input[id="tp-sign-in"]').click()
+        // cy.wait(20000)
+        cy.get('span[id="tp-message-error"]').should('be.visible')
+    })
+})
